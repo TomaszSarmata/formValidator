a50aaedbbd8a56146bd760291f241bdd5b90bfd1
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-22 10:10:24 +0000

    refactored the code inside our event listener to include more compact one that is based on just one function called checkRequired that takes in an array of inputs. Then added another function that will help us to grab an input id for the error message where the first letter of the input field name is capitalised

--- script.js
+++ script.js
@@ -35,36 +35,56 @@ function isValidEmail(email) {
 //      return re.test(String(email).toLowerCase());
 //   }
 
+//here we are going to create a generic function called checkRequired that will replace the entre string of if statements in our event listener to make sure the code looks clean and is scallable. We are going to pass it an array of inputs
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+//here we are going to create a function that will capitalise the first letter of the input id and join it with the rest of the id name to return an input id/name for the error message
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // adding event listiner on the entire form for the submission
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (!username.value) {
-    showError(username, "username is required");
-  } else if (username.value.length <= 2) {
-    showError(username, "username should be at least 3 characters long");
-  } else {
-    showSuccess(username);
-  }
-  if (!email.value) {
-    showError(email, "Email is required");
-  } else if (!isValidEmail(email.value)) {
-    showError(email, "Please type in a valid email address");
-  } else {
-    showSuccess(email);
-  }
-  if (!password.value) {
-    showError(password, "Password is required");
-  } else if (password.value.length <= 2) {
-    showError(password, "Password should be at least 3 characters long");
-  } else {
-    showSuccess(password);
-  }
-  if (!password2.value) {
-    showError(password2, "Password2 is required");
-  } else if (password2.value.length <= 2) {
-    showError(password2, "Password2 should be at least 3 characters long");
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
+
+  //the code below with a bunch of if statements was not scallable so had to be reformated
+  //   if (!username.value) {
+  //     showError(username, "username is required");
+  //   } else if (username.value.length <= 2) {
+  //     showError(username, "username should be at least 3 characters long");
+  //   } else {
+  //     showSuccess(username);
+  //   }
+  //   if (!email.value) {
+  //     showError(email, "Email is required");
+  //   } else if (!isValidEmail(email.value)) {
+  //     showError(email, "Please type in a valid email address");
+  //   } else {
+  //     showSuccess(email);
+  //   }
+  //   if (!password.value) {
+  //     showError(password, "Password is required");
+  //   } else if (password.value.length <= 2) {
+  //     showError(password, "Password should be at least 3 characters long");
+  //   } else {
+  //     showSuccess(password);
+  //   }
+  //   if (!password2.value) {
+  //     showError(password2, "Password2 is required");
+  //   } else if (password2.value.length <= 2) {
+  //     showError(password2, "Password2 should be at least 3 characters long");
+  //   } else {
+  //     showSuccess(password2);
+  //   }
 });
