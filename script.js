7bd457dd80fdcedd250ba1b2bd7edd4faee9a4dd
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-22 08:23:42 +0000

    created a showError helper function that we are using in our event listener

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+//helper functions
+function showError(input, message) {
+  //grabbing the parent element to add a class
+  const formControl = input.parentElement;
+  //now let's manipulate the classes so
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
 // adding event listiner on the entire form for the submission
 form.addEventListener("submit", function (e) {
   e.preventDefault();
